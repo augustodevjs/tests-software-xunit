bdb77f43ef902cf3eb7951dc7fa047b67ad3d906
augusto <augustosaaude@gmail.com>
2024-04-24 19:59:03 -0300

    feat: add first tests

---
+++ TestsSoftware/01 - Unity Tests/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Demo.Tests;
+
+public class CalculadoraTests
+{
+    [Fact]
+    public void Calculadora_Somar_RetornarValorSomado()
+    {
+        // Arrange
+        var calculadora = new Calculadora();
+
+        // Act
+        var resultado = calculadora.Somar(4, 2);
+
+        // Assert
+        Assert.Equal(6, resultado);
+    }
+
+    [Theory]
+    [InlineData(3, 0, 3)]
+    [InlineData(3, 5, 8)]
+    [InlineData(2, 5, 7)]
+    [InlineData(1, 5, 6)]
+    [InlineData(3, 2, 5)]
+    [InlineData(3, 15, 18)]
+    public void Calculadora_Somar_RetornarSomaValoresCorretos(double v1, double v2, double total)
+    {
+        // Arrange
+        var calculadora = new Calculadora();
+
+        // Act
+        var resultado = calculadora.Somar(v1, v2);
+
+        // Assert
+        Assert.Equal(total, resultado);
+    }
+}
