afe6c3df426cc33fe1c811a7446f56dcf928300b
augusto <augustosaaude@gmail.com>
2024-04-27 16:43:49 -0300

    feat: add mock

---
+++ TestsSoftware/02 - Features/Feature.Tests/04 - Dados Humanos/ClienteBogusTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Feature.Tests._04___Dados_Humanos;
+
+[Collection(nameof(ClienteBogusCollection))]
+public class ClienteBogusTests
+{
+    private readonly ClienteBogusFixture _clienteBogusFixture;
+
+    public ClienteBogusTests(ClienteBogusFixture clienteBogusFixture)
+    {
+        _clienteBogusFixture = clienteBogusFixture;
+    }
+
+    [Fact(DisplayName = "Novo Cliente Válido")]
+    [Trait("Categoria", "Cliente Bogus Testes")]
+    public void Cliente_NovoCliente_DeveEstarValido()
+    {
+        // Arrange
+        var cliente = _clienteBogusFixture.GerarClienteValido();
+
+        // Act
+        var result = cliente.EhValido();
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+    }
+}
