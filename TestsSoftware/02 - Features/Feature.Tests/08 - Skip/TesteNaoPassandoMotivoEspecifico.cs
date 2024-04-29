3ca3b57a4fffcb03d728efb8b300cbfb4098eea5
augusto <augustosaaude@gmail.com>
2024-04-29 18:38:53 -0300

    feat: add skip tests

---
+++ TestsSoftware/02 - Features/Feature.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Feature.Tests._08___Skip;
+public class TesteNaoPassandoMotivoEspecifico
+{
+    [Fact(DisplayName = "Novo Cliente 2.0", Skip = "Nova versão 2.0 quebrando")]
+    [Trait("Categoria", "Escapando dos Testes")]
+    public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+    {
+        Assert.True(false);
+    }
+}
